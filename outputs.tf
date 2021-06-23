134667646d41e68c817992444927cf62dee4d5f3
Manu <emmanuel.colussi@hpe.com>
2021-06-24 00:58:19 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "ec2_machines" {
-  value = aws_instance.worker-nodes.*.public_ip   
+output "worker_public_ip" {
+  value = aws_instance.worker-nodes.*.public_ip
 }
 
-output "instance_ips" {
-  value = aws_instance.master-nodes.*.public_ip
+output "master_public_ip" {
+  value = aws_instance.master-nodes.public_ip
 }
