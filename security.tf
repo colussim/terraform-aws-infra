7990138232ccf1ce8ea15d93e4220de6d67bfeea
Manu <emmanuel.colussi@hpe.com>
2021-07-06 10:41:57 +0200

    Update security.tf

--- security.tf
+++ security.tf
@@ -7,9 +7,29 @@ resource "aws_security_group" "sg_infra" {
    from_port = 22
    to_port = 22
    protocol = "tcp"
-   cidr_blocks = ["0.0.0.0/0"] 
+   cidr_blocks = ["0.0.0.0/0"]
  }
-  
+ ingress {
+    from_port = 6443
+    to_port = 6443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+      from_port = 443
+      to_port = 443
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
  ingress {
    from_port = -1
    to_port = -1
@@ -23,4 +43,3 @@ resource "aws_security_group" "sg_infra" {
       cidr_blocks = ["0.0.0.0/0"]
  }
 }
-
